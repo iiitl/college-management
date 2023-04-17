94f56ad2f55840027f1ff12785e9983d9aaec248
umeshSinghVerma <usv0308@gmail.com>
2023-04-17 10:30:25 +0530

    Call to Action using Components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-
+import CallToAction from "./components/homepage/callToAction/CallToAction.jsx"
 import HomePage from "./pages/home/Home.page.jsx"
 
 const App = () => {
   return (
+    <>
     <Router>
       <Routes>
-        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/"} element={<><HomePage /><CallToAction /></>} />
       </Routes>
     </Router>
+    
+    </>
   )
 }
 
--- client/src/pages/home/Home.page.jsx
+++ client/src/pages/home/Home.page.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Styles from "./Home.module.scss"
 import Header from "../../components/homepage/header/Header"
+import CallToAction from "../../components/homepage/callToAction/CallToAction"
 
 const HomePage = () => {
   return (
